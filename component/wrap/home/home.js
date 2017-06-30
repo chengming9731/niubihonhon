424d4b5f054edb4f821f85bc471abce7a3c9b0ca
xushijie <xushijie@xushijiedeMacBook-Pro.local>
2017-06-30 08:51:51 +0800

    徐士杰6月29号

--- component/wrap/home/home.js
+++ component/wrap/home/home.js
@@ -4,7 +4,14 @@ define(['uiRouter'],function(){
 			$stateProvider.state('wrap.home',{
 				url:'/home',
 				templateUrl:'component/wrap/home/home.html',
+				controller:'homeCtrl',
 				css:'component/wrap/home/home.css'
 			});
-		});
+		})
+		.controller('homeCtrl',['$scope','$http',function($scope,$http) {
+			$scope.name = '徐士杰';
+			$http.get('http://mce.mogucdn.com/jsonp/multiget/3?pids=51822%2C51827%2C41119%2C51833%2C51836%2C4604&callback=CALLBACK').then(function (res) {
+				console.log(res);
+        })
+		}])
 });
